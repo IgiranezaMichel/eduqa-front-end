6cca93d3dfb1688a6eb324b36f990ef0cc85f6b2
michel.igiraneza@auca.ac.rw <michel.igiraneza@auca.ac.rw>
2024-09-01 13:31:03 -0400

    add hod route

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import {Route, BrowserRouter as Router, Routes } from "react-router-dom"
 import { ForgetPassword } from "./page/visitor/authentication/forgetPassword"
 import { Login } from "./page/visitor/authentication/login"
+import { HOD } from "./page/hod"
 function App() {
 
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login/>}></Route>
         <Route path="/forgot-password" element={<ForgetPassword/>}></Route>
+        <Route path="/navigation" element={<HOD/>}></Route>
       </Routes>
       </div>
     </Router>
