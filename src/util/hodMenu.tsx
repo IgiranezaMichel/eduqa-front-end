6c9c8d0a115ce70a89fb37c9b5b581ad331550ba
michel.igiraneza@auca.ac.rw <michel.igiraneza@auca.ac.rw>
2024-09-01 18:22:23 -0400

    add hod menus

---
+++ src/util/hodMenu.tsx
@@ -0,0 +1,23 @@
+import { Dashboard } from "@mui/icons-material";
+import { INavigationItems } from "../interface/navigationitems";
+
+export const HodMenu:INavigationItems[]=[
+    {icon:<Dashboard/>,link:'/hod',name:'Overview'},
+    {icon:<Dashboard/>,link:'/hod/lectures',name:'lectures'},
+    {icon:<Dashboard/>,link:'/hod/semesters',name:'Semesters'},
+    {icon:<Dashboard/>,link:'/hod/students',name:'Students'},
+    {icon:<Dashboard/>,link:'/hod/courses',name:'Courses'},
+    {icon:<Dashboard/>,link:'/hod/reports',name:'Reports'},
+    {icon:<Dashboard/>,link:'/hod/reports',name:'Reports'},
+    {icon:<Dashboard/>,link:'/hod/reports',name:'Reports'},
+    {icon:<Dashboard/>,link:'/hod/reports',name:'Reports'},
+    {icon:<Dashboard/>,link:'/hod/reports',name:'Reports'},
+    {icon:<Dashboard/>,link:'/hod/reports',name:'Reports'},
+    {icon:<Dashboard/>,link:'/hod/reports',name:'Reports'},
+    {icon:<Dashboard/>,link:'/hod/reports',name:'Reports'},
+    {icon:<Dashboard/>,link:'/hod/reports',name:'Reports'},
+    {icon:<Dashboard/>,link:'/hod/reports',name:'Reports'},
+    {icon:<Dashboard/>,link:'/hod/reports',name:'Reports'},
+    {icon:<Dashboard/>,link:'/hod/reports',name:'Reports'},
+    {icon:<Dashboard/>,link:'/hod/reports',name:'Reports'},
+]
