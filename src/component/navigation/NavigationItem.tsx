35734e12bb54d7a0a05b80cc5c1046f397e1ac2b
michel.igiraneza@auca.ac.rw <michel.igiraneza@auca.ac.rw>
2024-09-01 18:20:59 -0400

    add items

---
+++ src/component/navigation/NavigationItem.tsx
@@ -0,0 +1,18 @@
+import { FC} from "react";
+import { useNavigate } from "react-router-dom";
+import { INavigationItems } from "../../interface/navigationitems";
+
+export const NavigationItem:FC<INavigationItems>=(prop)=>{
+    const navigation=useNavigate();
+
+    return <>
+    <div onClick={()=>navigation(prop.link)} className= {`flex gap-3 ps-5 py-2 ${prop.className}`}>
+        <div>
+            {prop.icon}
+        </div>   
+        <div>
+            {prop.name}
+        </div>   
+        </div>
+    </>
+}
