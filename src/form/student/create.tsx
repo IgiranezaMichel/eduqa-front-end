2ac047cf5aa9822d255a8631f6ed90f936ec7016
michel.igiraneza@auca.ac.rw <amal.healthafrica@gmail.com>
2024-09-09 23:29:03 +0300

    change interface

--- src/form/student/create.tsx
+++ src/form/student/create.tsx
@@ -1,24 +1,26 @@
 import { FC, ReactNode, useState } from "react";
 import { Button, FormControl, InputLabel, MenuItem, Select, TextField } from "@mui/material";
-import { IStudent } from "../../interface/student";
+import { IUser } from "../../interface/user";
+import { Role } from "../../enum/role";
 interface IStudentItem {
-    student: IStudent,
+    student: IUser,
     children: ReactNode
 
 }
 
 
 export const CreateStudent: FC<IStudentItem> = (prop) => {
-    const [student, setStudent] = useState<IStudent>({
-        firstName: prop.student.firstName,email: prop.student.email,
-        lastName: prop.student.lastName,
+    const [student, setStudent] = useState<IUser>({
+        name: prop.student.name, 
+        email: prop.student.email,
         gender: prop.student.gender,
-        regNumber: prop.student.regNumber,
-        profile: prop.student.profile,
-        semester: prop.student.semester,
-        phone:'',
-        id:prop.student.id
- 
+        id: prop.student.id,
+        password: prop.student.password,
+        phoneNumber: prop.student.phoneNumber,
+        picture: prop.student.picture,
+        id: prop.student.id,
+        role:Role.ROLE_STUDENT
+
     })
 
 
@@ -30,19 +32,11 @@ export const CreateStudent: FC<IStudentItem> = (prop) => {
                 {prop.children}
             </div>
             <form className="p-2">
-                <TextField required label='First Name' value={student.firstName}
-                    onChange={(e) => setStudent({ ...student, firstName: e.target.value })} className="mb-5" fullWidth />
+                <TextField required label='First Name' value={student.name}
+                    onChange={(e) => setStudent({ ...student, name: e.target.value })} className="mb-5" fullWidth />
 
-<TextField required label='First Name' value={student.firstName}
-                    onChange={(e) => setStudent({ ...student, firstName: e.target.value })} className="mb-5" fullWidth />
-
-<TextField required label='First Name' value={student.firstName}
-                    onChange={(e) => setStudent({ ...student, firstName: e.target.value })} className="mb-5" fullWidth />
-
-<TextField required label='First Name' value={student.firstName}
-                    onChange={(e) => setStudent({ ...student, firstName: e.target.value })} className="mb-5" fullWidth />
- 
-
+                {/* <TextField required label='First Name' value={student.firstName}
+                    onChange={(e) => setStudent({ ...student, firstName: e.target.value })} className="mb-5" fullWidth /> */}
                 <FormControl fullWidth>
                     <InputLabel id="gender-simple-select-label">Select gender</InputLabel>
                     <Select
@@ -53,10 +47,16 @@ export const CreateStudent: FC<IStudentItem> = (prop) => {
                     >
                         <MenuItem value={'Male'}>Male</MenuItem>
                         <MenuItem value={'Female'}>Female</MenuItem>
-                     </Select>
+                    </Select>
                 </FormControl>
+                <TextField type="email" required label='Email' value={student.email}
+                    onChange={(e) => setStudent({ ...student, email: e.target.value })} className="mb-5" fullWidth />
+
+                <TextField required label='First Name' value={student.phoneNumber}
+                    onChange={(e) => setStudent({ ...student, firstName: e.target.value })} className="mb-5" fullWidth />
+
                 <div className="py-2 ">
-                <Button className="bg-blue-950/90 w-full normal-case text-white p-3 font-bold text-xl">Add new Student</Button>
+                    <Button className="bg-blue-950/90 w-full normal-case text-white p-3 font-bold text-xl">Add new Student</Button>
                 </div>
             </form>
         </div>
