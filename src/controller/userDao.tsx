bee13e8e7de4a6cd3626b574940b9c6c8bca0cce
michel.igiraneza@auca.ac.rw <amal.healthafrica@gmail.com>
2024-09-09 22:38:04 +0300

    add user dao

---
+++ src/controller/userDao.tsx
@@ -0,0 +1,26 @@
+import { IPage } from "../interface/page";
+import { IUser } from "../interface/user";
+import { Axios } from "../util/axios";
+
+export class UserDao {
+    public registerUser=(user:IUser)=>{
+        const form=new FormData();
+        form.append("name",user.name);
+        form.append("gender",user.gender);
+        form.append("picture",user.picture);
+        form.append("role",user.role);
+        form.append("email",user.email);
+        form.append("password",user.password);
+        form.append("phoneNumber",user.phoneNumber);
+        if(user.id){
+            form.append("id",user.id);
+        }
+        return Axios().post("/user/register",form);
+    }
+    public login=(email:string,password:string)=>{
+        return Axios().post("/user/login",{email,password});
+    }
+    public getAllUserPage=(page:IPage)=>{
+        return Axios().get("/user/get/all",{params:page});
+    }
+}
