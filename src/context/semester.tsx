5935ecdb2fd6482462b1265eb3ed106f65b86886
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-09-10 09:03:58 +0100

    add semester

---
+++ src/context/semester.tsx
@@ -0,0 +1,34 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { IState } from "../interface/state";
+import { IPage } from "../interface/page";
+import { SemesterDao } from "../controller/semesterdao";
+
+const SemesterContext = createContext<IState|undefined>(undefined);
+export const useSemesterContext=()=>{
+    const Semester=useContext(SemesterContext);
+    if(!Semester){
+        throw new Error("useSemesterContext must be used within a SemesterProvider");
+    }
+    return Semester;
+}
+
+export const SemesterProvider = ({children}:any)=>{
+    const [page, setPage] = useState<IPage>({
+        pageNumber:0,pageSize:10,search:'',sortBy:'id'
+    });
+    const [refresh, setRefresh] = useState<boolean>(false);
+    const [content, setContent] = useState<any>([]);
+    useEffect(
+        ()=>{
+            new SemesterDao().getAllSemesterPage(page).then((data)=>{
+                setContent(data.data);
+            });
+        },[page,refresh]
+    );
+    const contextDate:IState={
+        content:content,
+        refresh:()=>setRefresh(!refresh),
+        update:(data:IPage)=>setPage(data)
+    }
+    return <SemesterContext.Provider value={contextDate}>{children}</SemesterContext.Provider>
+}
