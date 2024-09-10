54df7749715ade6f4bcb5e4a7d64bc2516ee0fa8
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-09-10 10:53:39 +0100

    add user provider

--- src/page/hod/student/index.tsx
+++ src/page/hod/student/index.tsx
@@ -2,12 +2,13 @@ import { Navigation } from "../../../component/navigation"
 import { HodMenu } from "../../../util/hodMenu"
 import { UserProvider } from "../../../context/user"
 import { DisplayStudent } from "./display"
+import { Role } from "../../../enum/role"
 
 export const HodManageStudent = () => {
 
 
     return <Navigation items={HodMenu}>
-        <UserProvider>
+        <UserProvider role={Role.ROLE_STUDENT}>
             <DisplayStudent />
         </UserProvider>
     </Navigation>
