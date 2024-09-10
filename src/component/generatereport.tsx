477349b640aa5acf9cd08ddbc8f9fcf6234c6f7d
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-09-10 10:51:57 +0100

    add generate report

---
+++ src/component/generatereport.tsx
@@ -0,0 +1,26 @@
+import jsPDF from 'jspdf';
+import 'jspdf-autotable';
+
+export const generateReport =(head: string,colums:any[],rows: any[],printedBy:string) => {
+    const doc = new jsPDF();
+    doc.text(head, 20, 10);
+    doc.text(printedBy, 20, 20);
+    // Table column titles
+    // const columns = ['Semester Name', 'Start Date', 'End Date'];
+
+    // Table row data
+    // const rows = [
+    //   ['Semester 1', '2024-01-01', '2024-05-31'],
+    //   ['Semester 2', '2024-06-01', '2024-12-31'],
+    //   ['Semester 3', '2025-01-01', '2025-05-31'],
+    // ];
+
+    // Generate the table
+    doc.autoTable({
+      head: [colums],
+      body: rows,
+      startY: 20,
+    });
+    doc.save(head+'.pdf');
+  };
+
