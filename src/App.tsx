218ac0ecb18cec046a2b6f9a5b1db16123cccc31
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-09-14 23:16:20 +0200

    add HodManage department

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { HodManageStudent } from "./page/hod/student"
 import { HodManageCourses } from "./page/hod/courses"
 import { HodManageSemester } from "./page/hod/semester"
 import { HodManageReport } from "./page/hod/report"
+import { HodManageDepartment } from "./page/hod/department"
 function App() {
 
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path="/" element={<Login/>}></Route>
         <Route path="/forgot-password" element={<ForgetPassword/>}></Route>
         <Route path="/hod" element={<HOD/>}></Route>
+        <Route path="/hod/department" element={<HodManageDepartment/>}></Route>
         <Route path="/hod/lectures" element={<HodManageLecturers/>}></Route>
         <Route path="/hod/students" element={<HodManageStudent/>}></Route>
         <Route path="/hod/courses" element={<HodManageCourses/>}></Route>
