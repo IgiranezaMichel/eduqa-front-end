ce5a71d1f3c12e1d28140a56dbe8d00d53043de2
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-09-15 18:00:39 +0200

    add department dao

--- src/controller/departmentdao.tsx
+++ src/controller/departmentdao.tsx
@@ -1,3 +1,4 @@
+import { Role } from "../enum/role";
 import { IDepartment } from "../interface/department";
 import { Axios } from "../util/axios";
 
@@ -10,5 +11,11 @@ export class DepartmentDao{
         form.append('id',data.id);
         form.append('name',data.name);
         return Axios().post('/department/register',form,{headers: {'Content-Type': 'application/json'}});
+    }
+    public async getAlldepartment(){
+        return await Axios().get('/department/get/all');
+    }
+    public async findAllUserWithInDepartment(role:Role){
+        return await Axios().get(`/department/get/all/user?role=${role}`);
     }
 }
