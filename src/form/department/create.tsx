bee17a19aab4fa883a3c545f3178c6129157eaa4
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-09-15 18:01:24 +0200

    add regex

--- src/form/department/create.tsx
+++ src/form/department/create.tsx
@@ -8,38 +8,18 @@ interface IDepartmentItems {
     children: ReactNode
 
 }
-export const Createdepartment: FC<IDepartmentItems> = (prop) => {
+export const CreateDepartment: FC<IDepartmentItems> = (prop) => {
     const [department, setdepartment] = useState<IDepartment>({
         name: prop.department.name,
         id: prop.department.id,
     })
-    const yearRegx = /^\d{4}-\d{4}$/;
     const savedepartment = (e: any) => {
         e.preventDefault();
-        if (yearRegx.test(department.name)) {
-            if (Number(department.name.split("-")[0]) < Number(department.name.split("-")[1])) {
                 new DepartmentDao().createDepartment(department).then(
                     data => { toast.success(data.data); }
-                ).catch(err => toast.error(err.message))
-            }
-            else {
-                toast.error("Invalid year format " + department.name.split("-")[0] + " cant't be greater than " + department.name.split("-")[1])
-            }
-
-        } else toast.error("Invalid year format")
+                ).catch(err => {toast.error(err.response.data);console.log(err);
+                })
     }
-
-    const inputLegex = /^\d{0,4}(-\d{0,4})?$/;
-    const handleYear = (e: any) => {
-        let inputValue = e.target.value;
-        if (inputLegex.test(inputValue)) {
-            if (inputValue.length === 4 && inputValue.indexOf('-') === -1) {
-                inputValue += "-";
-            }
-            setdepartment({ ...department, name: inputValue });
-        }
-    };
-
     return <>
 
         <div>
@@ -48,7 +28,7 @@ export const Createdepartment: FC<IDepartmentItems> = (prop) => {
             </div>
             <form className="p-2" onSubmit={savedepartment}>
                 <TextField required label='department name' value={department.name}
-                    onChange={handleYear} className="mb-5" fullWidth />
+                    onChange={e=>setdepartment({ ...department, name: e.target.value })} className="mb-5" fullWidth />
                 <div className="py-2 ">
                     <Button type="submit" className="bg-blue-950/90 w-full normal-case text-white p-3 font-bold text-xl">Add new department</Button>
                 </div>
