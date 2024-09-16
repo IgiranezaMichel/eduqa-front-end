c895753b98a46db2e33a6733ca3857a5ae5b8d9d
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-09-16 16:22:57 +0200

    add login routes

--- src/page/visitor/authentication/login.tsx
+++ src/page/visitor/authentication/login.tsx
@@ -1,8 +1,22 @@
-import { Button, TextField } from "@mui/material"
+import { Button, CircularProgress, TextField } from "@mui/material"
+import { useState } from "react"
 import { Link } from "react-router-dom"
+import {toast, Toaster} from 'sonner'
+import { UserDao } from "../../../controller/userDao";
 export const Login=()=>{
+    const [userName,setUserName]=useState('');
+    const [password,setPassword]=useState('');
+    const [isLoading,setIsLoading]=useState(false)
+    const loginHandler=async(e:any)=>{
+        e.preventDefault();
+        setIsLoading(true);
+        new UserDao().login(userName,password)
+        .then(()=>{setIsLoading(false);}
+        )
+        .catch(err=>{setIsLoading(false);toast.error(err.response.data||err.message)})
+    }
     return <>
-    <section className="h-screen w-screen bg-slate-300 flex items-center justify-center">
+    <form onSubmit={loginHandler} className="h-screen w-screen bg-slate-300 flex items-center justify-center">
     <div className="grid grid-cols-2 items-center w-[70%] m-auto bg-white">
         <section className="p-10">
             <section>
@@ -12,13 +26,20 @@ export const Login=()=>{
             <section>
                 <div className="font-bold mb-4">
                     <label htmlFor="username" className="d-block mb-10">Email or username</label>
-                    <TextField placeholder="Enter your email or username" fullWidth/>
+                    <TextField
+                    onChange={e=>setUserName(e.target.value)} value={userName}
+                    placeholder="Enter your email or username" fullWidth/>
                 </div>
                 <div className="font-bold mb-4">
                     <label htmlFor="username" className="d-block mb-10">Password</label>
-                    <TextField placeholder="Enter your email or username" fullWidth/>
+                    <TextField
+                     type="password" onChange={e=>setPassword(e.target.value)} value={password}
+                    placeholder="Enter your password" fullWidth/>
                 </div>
-                <Button className="bg-blue-950/90 w-full normal-case text-white p-3 font-bold text-xl">Sign In</Button>
+                <Toaster position="bottom-left"/>
+                {!isLoading?<Button className="bg-blue-950/90 w-full normal-case text-white p-3 font-bold text-xl" type="submit">Sign In</Button>
+            :<div className="text-center"><CircularProgress className="m-auto text-center"/>  </div>  
+            }
                 <div>
                     Forgot Password ? <Link to={'forgot-password'}>reset password</Link>
                 </div>
@@ -38,7 +59,7 @@ export const Login=()=>{
            </section>
         </section>
     </div>
-    </section>
+    </form>
     
     </>
 }
