ddf0de92fa03c6e7f13529082c038c6f3de3a767
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-09-20 17:56:24 +0200

    SemesterUserProvider

---
+++ src/context/semesteruser.tsx
@@ -0,0 +1,35 @@
+import { createContext, ReactNode, useContext, useEffect, useState } from "react";
+import { IState } from "../interface/state";
+import { IPage } from "../interface/page";
+import { Role } from "../enum/role";
+import { RegistrationDao } from "../controller/registrationdao";
+
+const SemesterUserContext = createContext<IState|undefined>(undefined);
+export const useSemesterUserContext=()=>{
+    const SemesterUser=useContext(SemesterUserContext);
+    if(!SemesterUser){
+        throw new Error("useSemesterUserContext must be used within a SemesterUserProvider");
+    }
+    return SemesterUser;
+}
+
+export const SemesterUserProvider = (prop:{children:ReactNode,semesterId:string,role:Role})=>{
+    const [page, setPage] = useState<IPage>({
+        pageNumber:0,pageSize:10,search:'',sortBy:'id'
+    });
+    const [refresh, setRefresh] = useState<boolean>(false);
+    const [content, setContent] = useState<any>([]);
+    useEffect(
+        ()=>{
+            new RegistrationDao().getAvailabelUserRegisteredForASemesterPage(page,prop.semesterId,prop.role).then((data)=>{
+                setContent(data.data);
+            });
+        },[page,refresh,prop.semesterId,prop.role]
+    );
+    const contextDate:IState={
+        content:content,
+        refresh:()=>setRefresh(!refresh),
+        update:(data:IPage)=>setPage(data)
+    }
+    return <SemesterUserContext.Provider value={contextDate}>{prop.children}</SemesterUserContext.Provider>
+}
