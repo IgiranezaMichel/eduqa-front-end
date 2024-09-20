89a34502a4a141bb1e7993d8e637dc5a679e6426
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-09-20 17:59:08 +0200

    Display Course

--- src/page/admin/course/index.tsx
+++ src/page/admin/course/index.tsx
@@ -1,11 +1,12 @@
 import { Navigation } from "../../../component/navigation"
+import { CourseProvider } from "../../../context/course"
 import { AdminMenu } from "../../../util/admin"
+import { DisplayCourse } from "./display"
 
 export const AdminManageCourse=()=>{
     return <Navigation items={AdminMenu}>
-    <div>
-        
-    </div>
-    
+     <CourseProvider>
+        <DisplayCourse />
+    </CourseProvider>
     </Navigation>
 }
