2d4c1f98c663145f01877ec01a9d05db810610bb
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-09-22 23:50:36 +0200

    create LectureCourseDao

--- src/controller/lecturecourses.tsx
+++ src/controller/lecturecourses.tsx
@@ -2,19 +2,19 @@ import { ILectureCourse } from "../interface/lecturecourse";
 import { IPage } from "../interface/page";
 import { Axios } from "../util/axios";
 
-export class LectureCourseDao{
+export class LectureCourseDao {
     public async getLectureCourses(id: any) {
         return Axios().get(`/lecture-course/all/${id}`);
     }
-    public async createLectureCourse (lectureCourses: ILectureCourse) {
-        return Axios().post('/lecture-course/register',lectureCourses)
+    public async createLectureCourse(lectureCourses: ILectureCourse) {
+        return Axios().post('/lecture-course/register', lectureCourses)
     }
-    public async getAllLectureCourse(page:IPage){
-        return Axios().post(`/lecture-course/all/courses`,page)
+    public async getAllLectureCoursePage(page: IPage, semesterId: string) {
+        return Axios().post(`/lecture-course/all/courses/${semesterId}`, page, { withCredentials: true })
     }
-    
+
     public async countCourse() {
         return Axios().get(`/lecture-course/total/courses`)
     }
-    
+
 }
