31221954d067dd3e160842e080b93bb9d7cb715c
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-09-22 08:50:44 +0200

    getLectureCourses

--- src/controller/lecturecourses.tsx
+++ src/controller/lecturecourses.tsx
@@ -1,7 +1,14 @@
+import { ILectureCourse } from "../interface/lecturecourse";
 import { IPage } from "../interface/page";
 import { Axios } from "../util/axios";
 
 export class LectureCourseDao{
+    public async getLectureCourses(id: any) {
+        return Axios().get(`/lecture-course/all/${id}`);
+    }
+    public async createLectureCourse (lectureCourses: ILectureCourse) {
+        return Axios().post('/lecture-course/register',lectureCourses)
+    }
     public async getAllLectureCourse(page:IPage){
         return Axios().post(`/lecture-course/all/courses`,page)
     }
@@ -9,4 +16,5 @@ export class LectureCourseDao{
     public async countCourse() {
         return Axios().get(`/lecture-course/total/courses`)
     }
+    
 }
