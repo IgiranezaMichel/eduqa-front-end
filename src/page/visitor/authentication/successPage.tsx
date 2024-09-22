46403d16f2bde80f2aacae32c3dd606818b82297
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-09-22 23:57:39 +0200

    add SuccessAuthenticationPage

--- src/page/visitor/authentication/successPage.tsx
+++ src/page/visitor/authentication/successPage.tsx
@@ -1,9 +1,38 @@
-import { ReactNode, useEffect, useState } from "react"
+import { ReactNode, useEffect, useRef, useState } from "react"
 import { UserDao } from "../../../controller/userDao"
 import { AuthProvider } from "../../../context/authentication";
+import { LinearProgress } from "@mui/material";
 
 export const SuccessAuthenticationPage=()=>{
-    const [isLoading,setIsLoading]=useState(true)
+    const [isLoading,setIsLoading]=useState(true);
+    const [progress, setProgress] = useState(0);
+    const [buffer, setBuffer] = useState(10);
+  
+    const progressRef =useRef(() => {});
+     useEffect(() => {
+      progressRef.current = () => {
+        if (progress === 100) {
+          setProgress(0);
+          setBuffer(10);
+        } else {
+          setProgress(progress + 1);
+          if (buffer < 100 && progress % 5 === 0) {
+            const newBuffer = buffer + 1 + Math.random() * 10;
+            setBuffer(newBuffer > 100 ? 100 : newBuffer);
+          }
+        }
+      };
+    });
+  
+    useEffect(() => {
+      const timer = setInterval(() => {
+        progressRef.current();
+      }, 100);
+  
+      return () => {
+        clearInterval(timer);
+      };
+    }, []);
     useEffect(
         ()=>{
             const userData=new UserDao().successLogin();
@@ -15,8 +44,8 @@ export const SuccessAuthenticationPage=()=>{
     );
     
     return <AuthProvider>
-    {isLoading?<>
-    loading ...
-    </>:<></>}
+    {isLoading?<div className="h-screen w-screen flex justify-center items-center">
+      <LinearProgress variant="buffer" value={progress} valueBuffer={buffer} />
+    </div>:<></>}
     </AuthProvider>
 }
