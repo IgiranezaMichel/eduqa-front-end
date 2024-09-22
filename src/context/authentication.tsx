d8a9aaf38c5de5ca6e006dbd43a77b21c393e905
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-09-22 23:49:43 +0200

    refactor auth

--- src/context/authentication.tsx
+++ src/context/authentication.tsx
@@ -1,64 +1,68 @@
 import { ReactNode, createContext, useContext, useEffect, useState } from "react";
-import axios from "axios";
 import { useLocation, useNavigate } from "react-router-dom";
 import { IState } from "../interface/state";
 import { UserDao } from "../controller/userDao";
 import { Role } from "../enum/role";
 
-export const AuthContext=createContext<IState|undefined>(undefined);
-export const useAuthenticationContext=()=>{
-    const authenticated=useContext(AuthContext);
-    if(!authenticated)throw new Error("Not authenticated")
+export const AuthContext = createContext<IState | undefined>(undefined);
+export const useAuthenticationContext = () => {
+    const authenticated = useContext(AuthContext);
+    if (!authenticated) throw new Error("Not authenticated")
     return authenticated;
 }
-export const AuthProvider=(prop:{children:ReactNode})=>{
-    const location=useLocation();
-    const [user,setUser]=useState<any>({});
-    const [isLoading,setIsLoading]=useState(true);
-    const navigation=useNavigate();
-    const [refresh,setRefresh]=useState(false);
+export const AuthProvider = (prop: { children: ReactNode }) => {
+    const location = useLocation();
+    const [user, setUser] = useState<any>({});
+    const [isLoading, setIsLoading] = useState(true);
+    const navigation = useNavigate();
+    const [refresh, setRefresh] = useState(false);
     useEffect(
-        ()=>{
-        new UserDao().successLogin()
-        .then((data)=>{setUser(data.data);setIsLoading(false)})
-        .catch((err)=>{
-            console.log(err)
-            setIsLoading(false);
-            navigation("/");
-        })
-        if(!isLoading){
-       if(user.role==Role.ROLE_ADMIN){
-        if(location.pathname=="/")
-        navigation("/admin")
-    else{
-        navigation(location.pathname)
-    }
-       }
-       else if(user.role==Role.ROLE_HOD){
-        navigation("/hod")
-       }
-       else if(user.role==Role.ROLE_INSTRACTOR){
-        navigation("/instructor")
-       }
-       else if(user.role==Role.ROLE_STUDENT){
-        navigation("/student")
-       }
-       else{
-        navigation("/")
-       }
-        console.log(user)
-    }
-        },[location.pathname,refresh,isLoading])
-    const data:IState={
-        content:user,
-        update:()=>setUser(user),
-        refresh:()=>setRefresh(!refresh)
+        () => {
+            new UserDao().successLogin()
+                .then((data) => { setUser(data.data); setIsLoading(false) })
+                .catch((err) => {
+                    console.log(err)
+                    setIsLoading(false);
+                    navigation("/");
+                })
+            if (!isLoading) {
+                if (user.role == Role.ROLE_ADMIN) {
+                    if (location.pathname == "/")
+                        navigation("/admin")
+                    else {
+                        navigation(location.pathname)
+                    }
+                }
+                else if (user.role == Role.ROLE_HOD) {
+                    navigation("/hod")
+                }
+                else if (user.role == Role.ROLE_INSTRACTOR) {
+                    if (location.pathname == "/")
+                        navigation("/instructor")
+                    else {
+                        navigation(location.pathname)
+                    }
+
+                }
+                else if (user.role == Role.ROLE_STUDENT) {
+                    navigation("/student")
+                }
+                else {
+                    navigation("/")
+                }
+                console.log(user)
+            }
+        }, [location.pathname, refresh, isLoading])
+    const data: IState = {
+        content: user,
+        update: () => setUser(user),
+        refresh: () => setRefresh(!refresh)
     }
     return <>
-    {!isLoading? 
-        <AuthContext.Provider value={data}>
-        {prop.children}
-    </AuthContext.Provider>:<div>Is loading</div>
-    }
+        {!isLoading ?
+            <AuthContext.Provider value={data}>
+                {prop.children}
+            </AuthContext.Provider> : <div>Is loading</div>
+        }
     </>
 }
