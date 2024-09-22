8e7913e18b3594a3c2dfd8be874c15d8f87299b4
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-09-22 08:52:02 +0200

    add lectureStatus

--- src/page/hod/lectures/index.tsx
+++ src/page/hod/lectures/index.tsx
@@ -3,11 +3,21 @@ import { HodMenu } from "../../../util/hodMenu"
 import { Role } from "../../../enum/role"
 import { UserProvider } from "../../../context/user"
 import { DisplayLecture } from "./display"
+import { UserStatus } from "../../../enum/userStatus"
+import { useState } from "react"
 
 export const HodManageLecturers=()=>{
+const [lectureStatus,setLectureStatus]=useState(UserStatus.ACTIVE);
     return <Navigation items={HodMenu}>
-    <UserProvider role={Role.ROLE_INSTRACTOR}>
-        <DisplayLecture />
+      
+    <UserProvider status={lectureStatus} role={Role.ROLE_INSTRACTOR}>
+        <DisplayLecture selectStatus={  <section>
+                <select onChange={e=>e.target.value.length!=0&&setLectureStatus(e.target.value as UserStatus)} className="p-1">
+                    <option value="">select status</option>
+                    <option value={UserStatus.ACTIVE}>active</option>
+                    <option value={UserStatus.INACTIVE}>inactive</option>
+                </select>
+            </section>}/>
     </UserProvider>
 </Navigation>
 }
