9a7c1fbbcff089a553468783558a277828bbb6b2
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-09-22 23:53:36 +0200

    add getListregisteredStudentForLectureCourses

---
+++ src/controller/studentregistercourse.tsx
@@ -0,0 +1,8 @@
+import { IPage } from "../interface/page";
+import { Axios } from "../util/axios";
+
+export class StudentRegisterCourseDao {
+    public  async getListregisteredStudentForLectureCourses(semesterId:string, page:IPage) {
+        return Axios().post(`/registered-student/get/student/${semesterId}`,page);
+    }
+}
