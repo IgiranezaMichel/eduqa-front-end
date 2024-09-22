c8557d8438d326ae6b0eebff8e8b4bdb99af558a
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-09-22 23:49:59 +0200

    create LectureCourseContext

---
+++ src/context/lecturecourse.tsx
@@ -0,0 +1,33 @@
+import { createContext, ReactNode, useContext, useEffect, useState } from "react";
+import { IState } from "../interface/state";
+import { LectureCourseDao } from "../controller/lecturecourses";
+import { IPage } from "../interface/page";
+
+const LectureCourseContext = createContext<IState | undefined>(undefined);
+export const useLectureCourseContext = () => {
+    const LectureCourse = useContext(LectureCourseContext);
+    if (!LectureCourse) {
+        throw new Error("useLectureCourseContext must be used within a LectureCourseProvider");
+    }
+    return LectureCourse;
+}
+export const LectureCourseProvider = (prop:{children:ReactNode,semesterId:string}) => {
+    const [page, setPage] = useState<IPage>({
+        pageNumber: 0, pageSize: 10, search: '', sortBy: 'id'
+    });
+    const [refresh, setRefresh] = useState<boolean>(false);
+    const [content, setContent] = useState<any>([]);
+    useEffect(
+        () => {
+            new LectureCourseDao().getAllLectureCoursePage(page,prop.semesterId).then((data) => {
+                setContent(data.data);
+            });
+        }, [page, refresh,prop.semesterId]
+    );
+    const contextDate: IState = {
+        content: content,
+        refresh: () => setRefresh(!refresh),
+        update: (data: IPage) => setPage(data)
+    }
+    return <LectureCourseContext.Provider value={contextDate}>{prop.children}</LectureCourseContext.Provider>
+}
