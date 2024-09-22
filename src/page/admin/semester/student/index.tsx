36ec30244e995cd76c2480e9f9a5358a49a873f2
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-09-22 23:55:01 +0200

    add DisplayStudent

--- src/page/admin/semester/student/index.tsx
+++ src/page/admin/semester/student/index.tsx
@@ -1,20 +1,9 @@
 
-import { useEffect, useState } from "react"
 import { SemesterUserProvider } from "../../../../context/semesteruser"
 import { Role } from "../../../../enum/role"
-import { SemesterDao } from "../../../../controller/semesterdao"
 import StudentHomeTable from "./display"
- export const DisplayStudent = () => {
-const [role, setRole] = useState<Role>(Role.ROLE_STUDENT)
-const [semester, setSemester] =useState<any>({});
-useEffect(
-  ()=>{
-      new SemesterDao().getCurrentSemester().then((semester)=>{
-          setSemester(semester.data);
-      })
-  },[]
-)  
-     return <SemesterUserProvider role={role} semesterId={semester.id}>
-       <StudentHomeTable semester={semester}/>
+ export const DisplayStudent = (prop:{role:Role,semester:any}) => {
+     return <SemesterUserProvider role={prop.role} semesterId={prop.semester.id}>
+       <StudentHomeTable semester={prop.semester}/>
     </SemesterUserProvider>
 }
