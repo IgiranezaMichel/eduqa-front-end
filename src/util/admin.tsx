a160d3207df90640470957eb3db858f22a4e9695
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-09-22 23:57:55 +0200

    remove route

--- src/util/admin.tsx
+++ src/util/admin.tsx
@@ -1,11 +1,10 @@
-import { BookSharp, Dashboard, Description, Folder, LockPerson, PeopleAlt } from "@mui/icons-material";
+import { BookSharp, Dashboard, Description, Folder, PeopleAlt } from "@mui/icons-material";
 import { INavigationItems } from "../interface/navigationitems";
 
 export const AdminMenu:INavigationItems[]=[
     {icon:<Dashboard/>,link:'/admin',name:'Overview'},
     {icon:<PeopleAlt/>,link:'/admin/user',name:'Users'},
     {icon:<Folder/>,link:'/admin/semester',name:'Semesters'},
-    {icon:<LockPerson/>,link:'/admin/student',name:'Students'},
     {icon:<BookSharp/>,link:'/admin/course',name:'Courses'},
     {icon:<Description/>,link:'/admin/report',name:'Reports'},
 
