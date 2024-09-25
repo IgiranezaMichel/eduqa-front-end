a8fecfcf6dc113b07dfbfed5f4c9b85d7a22e97b
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-09-25 11:57:04 +0200

    add ShowUserProfile

--- src/page/student/course/index.tsx
+++ src/page/student/course/index.tsx
@@ -1,10 +1,12 @@
 import { TextField } from "@mui/material"
 import { Navigation } from "../../../component/navigation"
 import { StudentMenu } from "../../../util/studentMenu"
-import { DisplayCourse } from "./display"
 import { SemesterDao } from "../../../controller/semesterdao"
 import { useEffect, useState } from "react"
 import { toast } from "sonner"
+import { DisplayCourse } from "./display/all"
+import { OnQueueCourses } from "./display/queu"
+import { DisplaySemesterCourses } from "./display/semester"
 
 export  const StudentCourse = () => {
     const [semester,setSemester]=useState<any>({})
@@ -26,29 +28,24 @@ export  const StudentCourse = () => {
             <h1 className="font-bold text-blue-950 text-xl">List Course of courses</h1>
             <div className="text-sm">This table contains list of courses</div>
             </div>
-             <div className="flex items-center justify-between mb-1 pb-2 border-b-2 border-b-blue-950">
-                <TextField 
-                sx={{ '& .MuiInputBase-root': { height: '40px', }, }}
-                    placeholder="Search"/>
-                    
-                <div className="flex gap-3">
-                <select name="" id="" className="p-2">
-                    <option value="">Select Semester</option>
-                </select>
-                <select name="" id="">
-                    <option value="">Select Semester Number</option>
-                </select>
-                </div>
-             </div>
-             <div className="flex gap-4 mb-4">
+             
+             <div className="flex gap-4 mb-2 py-1 border-b-2 border-b-blue-950">
                 <button onClick={()=>setActive("all")} className={active=='all'?activeClass:inactiveClass}>All</button>
+                <button onClick={()=>setActive("queue")} className={active=='queue'?activeClass:inactiveClass}>Course on queue</button>
                 <button onClick={()=>setActive("sem")} className={active=='sem'?activeClass:inactiveClass}>
                 <i className="text-sm">{semester.semesterName}</i> 
             {' '}{semester.semNumber=1?'I':semester.semNumber=2?'II':semester.semNumber=3?'III':semester.semNumber=4?'IV':semester.semNumber=5?'V':semester.semNumber=6?'VI':semester.semNumber=7?'VII':semester.semNumber=8?'VIII':''} courses        
 
                 </button>
              </div>
-             <DisplayCourse/>
+             <div className="flex items-center justify-between mb-1 pb-2">
+                <TextField 
+                sx={{ '& .MuiInputBase-root': { height: '40px', }, }}
+                    placeholder="Search"/>
+             </div>
+             {active=='all'&&<DisplayCourse/>}
+             {active=='queue'&&<OnQueueCourses/>}
+             {active=='sem'&&<DisplaySemesterCourses semesterId={semester.id}/>}
         </Navigation>
     )
 }
