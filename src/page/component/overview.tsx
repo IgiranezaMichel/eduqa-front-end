715147a4aae3177420ae773a6e47d9b88bfef5eb
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-09-25 11:56:39 +0200

    add open dialog

--- src/page/component/overview.tsx
+++ src/page/component/overview.tsx
@@ -1,11 +1,18 @@
-import { Visibility } from "@mui/icons-material"
+import { Close, Visibility } from "@mui/icons-material"
+import { useAuthenticationContext } from "../../context/authentication"
+import { Dialog, IconButton } from "@mui/material"
+import { ShowUserProfile } from "./userdetails"
+import { useState } from "react"
 export const OverviewHeading=()=>{
+    const {content}=useAuthenticationContext()
+    const [openDialog,setOpenDialog]=useState(false)
     return <div>
     <div className="rounded-xl bg-blue-900 bg-blue-950/90">
         <div className="flex text-white p-5">
             <section>
                 <div className="mb-4 text-xl">
                 Welcome ,<span className="text-yellow-400">
+                    {content.name}
                     </span>
                 </div> 
                 <div className="text-sm">
@@ -14,7 +21,7 @@ export const OverviewHeading=()=>{
                 re, ipsum dolor sit omet consectecture
                 Lore, ipsum dolor sit omet consectecture
                 </div>
-                <button 
+                <button onClick={()=>setOpenDialog(true)}
                  className="mt-4 flex gap-2 p-2 text-white rounded-xl border border-white">
                     <Visibility className="text-white"/> View Profile
                  </button>
@@ -22,5 +29,10 @@ export const OverviewHeading=()=>{
                  <img src="../overview-index.png" className="w-[20%]" alt="" />
          </div>
     </div>
+    <Dialog open={openDialog} onClose={()=>setOpenDialog(false)}>
+      <ShowUserProfile user={content}>
+        <div className="flex justify-between font-bold py-2 ms-4 items-center">Details <IconButton onClick={()=>setOpenDialog(false)}><Close/></IconButton></div>
+      </ShowUserProfile>
+    </Dialog>
 </div>
 }
