ab5f5c86e32ad34e883db9f8eae36e4dd39d2b3c
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-09-25 11:56:19 +0200

    add StudentRegisterCourseDao methods

--- src/controller/studentregistercourse.tsx
+++ src/controller/studentregistercourse.tsx
@@ -8,4 +8,13 @@ export class StudentRegisterCourseDao {
     public  async getStudentCourses(page:IPage) {
         return Axios().post(`/registered-student/get/student/courses`,page);
     }
+    public async getStudentPrincipalCourses(){
+        return Axios().get('/registered-student/get/student/courses')
+    }
+    public async getStudentPrincipalCompletedCourseHistory(){
+        return Axios().get('/registered-student/getall/student/courses')
+    }
+    public async getAllStudentRegisteredCourseWithInAsemester(semesterId:string){
+        return Axios().get(`/registered-student/getall/student/registered/courses/${semesterId}`)
+    }
 }
