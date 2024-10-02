b04651b0d83406f44e3a51a9f7499718506488ad
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-10-02 20:06:20 +0200

    add spacing

--- src/form/semester/create.tsx
+++ src/form/semester/create.tsx
@@ -41,8 +41,7 @@ export const Createsemester: FC<ISemesterItem> = (prop) => {
             }
             setsemester({ ...semester, name: inputValue });
         }
-    };
-
+    };    
     return <>
 
         <div>
