f804c4c33b7a34d9d30b0dd5a232b39fb40d44d4
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-10-02 20:07:06 +0200

    add dialogs

--- src/page/admin/semester/index.tsx
+++ src/page/admin/semester/index.tsx
@@ -7,6 +7,9 @@ import { toast } from "sonner"
 import { DisplaySemesterCourses } from "./courses"
 import { DisplayStudent } from "./student"
 import { Role } from "../../../enum/role"
+import { Dialog, IconButton } from "@mui/material"
+import { Createsemester } from "../../../form/semester/create"
+import { Add, Close } from "@mui/icons-material"
 export const AdminManageSemester = () => {
     const [semesterList, setSemesterList] = useState([]);
     const [currentSemester, setCurrentSemester] = useState<any>({});
@@ -17,52 +20,65 @@ export const AdminManageSemester = () => {
             }).catch((err) => {
                 toast.error(err.message);
             });
-        },[]
+        }, []
     )
     useEffect(
         () => {
-            
+
             new SemesterDao().getCurrentSemester()
-            .then((res) => {
-                setCurrentSemester(res.data)
-            }).catch((err) => {
-                toast.error(err.message);
-            })
+                .then((res) => {
+                    setCurrentSemester(res.data)
+                }).catch((err) => {
+                    toast.error(err.message);
+                })
         }, []
     );
-    const handleSemesterChange = (e:any) => {
-        new SemesterDao().getSemester(e.target.value).then(data=>
-        setCurrentSemester(data.data))
+    const handleSemesterChange = (e: any) => {
+        new SemesterDao().getSemester(e.target.value).then(data =>
+            setCurrentSemester(data.data))
     }
-    const [activeBtn,setActiveBtn] = useState("student");
-    const activeClass= "bg-blue-900 text-white border border-gray-300 p-2 rounded";
+    const [openDialog, setOpenDialog] = useState({ open: false, type: 'create' });
+    const [activeBtn, setActiveBtn] = useState("student");
+    const activeClass = "bg-blue-900 text-white border border-gray-300 p-2 rounded";
     const inactiveClass = "border border-gray-300 p-2 rounded";
     return <Navigation items={AdminMenu}>
         <div className="flex items-center justify-between clear-both pt-3">
-            <section>
-                <div className="font-bold text-xl">
-                    Semesters
-                </div>
-            </section>
-            <section className="flex items-center gap-3">
-               
-            </section>
+            <div className="font-bold text-xl">
+                Semesters
+            </div>
+            <button onClick={() => setOpenDialog({ open: true, type: 'create' })} className="bg-blue-900 text-white p-2 rounded-md"><Add /></button>
         </div>
         <HodOverview />
         <section className="flex items-center justify-between  gap-2 bg-blue-200">
-           <div>
-           <button className={activeBtn === "student" ? activeClass : inactiveClass} onClick={() => setActiveBtn("student")}>Registered student</button>
-            <button className={activeBtn==="lecture"?activeClass:inactiveClass} onClick={() => setActiveBtn("lecture")}>Available Lecture</button>
-            <button className={activeBtn === "course" ? activeClass : inactiveClass} onClick={() => setActiveBtn("course")}>Available Courses</button>
-           </div>
+            <div>
+                <button className={activeBtn === "student" ? activeClass : inactiveClass} onClick={() => setActiveBtn("student")}>Registered student</button>
+                <button className={activeBtn === "lecture" ? activeClass : inactiveClass} onClick={() => setActiveBtn("lecture")}>Available Lecture</button>
+                <button className={activeBtn === "course" ? activeClass : inactiveClass} onClick={() => setActiveBtn("course")}>Available Courses</button>
+            </div>
             <select onChange={handleSemesterChange} name="" id="" className="p-1 mx-1 rounded border border-gray-300">
-                    <option value="">Semester filtering</option>
-                    {semesterList!=undefined&&semesterList.length!=0&&semesterList.map((item: any, index: number) => <option key={item.id + index} value={item.id}>{item.semesterName}</option>)
-                    }
-                </select>
+                <option value="">Semester filtering</option>
+                {semesterList != undefined && semesterList.length != 0 && semesterList.map((item: any, index: number) => <option key={item.id + index} value={item.id}>{item.semesterName}</option>)
+                }
+            </select>
         </section>
-        {activeBtn=='student'&&<DisplayStudent semester={currentSemester} role={Role.ROLE_STUDENT} />}
-        {activeBtn=='lecture'&&<DisplayStudent semester={currentSemester} role={Role.ROLE_INSTRACTOR} />}
-        {activeBtn=='course'&&<DisplaySemesterCourses />}
+        {activeBtn == 'student' && <DisplayStudent semester={currentSemester} role={Role.ROLE_STUDENT} />}
+        {activeBtn == 'lecture' && <DisplayStudent semester={currentSemester} role={Role.ROLE_INSTRACTOR} />}
+        {activeBtn == 'course' && <DisplaySemesterCourses />}
+        <Dialog maxWidth='xs' PaperProps={{ className: 'w-full' }} open={openDialog.open}>
+            <Createsemester semester={currentSemester}>
+                <section className="flex justify-between p-2 items-center mb-4">
+                    <div>
+                        {openDialog.type == 'create' ? <>
+                            <div className="text-blue-900/80 font-bold text-lg">Add new semester</div>
+                            <div className="text-sm text-slate-600">
+                                Add new semester to the list of semester fill the form below
+
+                            </div>
+                        </> : <>Update Course</>}
+                    </div>
+                    <IconButton className="bg-blue-200/50" onClick={() => setOpenDialog({ ...openDialog, open: false })}><Close /></IconButton>
+                </section>
+            </Createsemester>
+        </Dialog>
     </Navigation>
 }
