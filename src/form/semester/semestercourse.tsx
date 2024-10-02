87087dbb1d92ca25b8d4140ea00d77443231f1b4
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-10-02 20:10:29 +0200

    RegisterSemesterCourseForm

---
+++ src/form/semester/semestercourse.tsx
@@ -0,0 +1,7 @@
+import { ReactNode, useState } from "react";
+
+export const RegisterSemesterCourseForm=(prop:{registration:any,children:ReactNode})=>{
+    const [pendingCourse,setPendingCourse]=useState<any>([])
+    return <>
+    </>
+}
