ad5f7b33d3bc307b4f69293c5d10e03315efb7d5
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-10-04 06:32:25 +0200

    InstructorOverview

--- src/page/instructor/index/instructoroverview.tsx
+++ src/page/instructor/index/instructoroverview.tsx
@@ -3,15 +3,13 @@ import { useEffect, useState } from "react";
 import { toast } from "sonner";
 import { UserDao } from "../../../controller/userDao";
 import { Role } from "../../../enum/role";
-import { SemesterDao } from "../../../controller/semesterdao";
 import { DashboardCard } from "../../hod/index/component/DashboardCard";
 import { LectureCourseDao } from "../../../controller/lecturecourses";
 import { UserStatus } from "../../../enum/userStatus";
 
-export const InstructorOverview = () => {
+export const InstructorOverview = (prop:{semester:any}) => {
     const [totalStudents, setTotalStudents] = useState(0)
     const [totalCourses, setTotalCourses] = useState(0)
-    const [semester, setSemester] = useState<any>({})
     useEffect(() => {
         new UserDao().countUserByRole(Role.ROLE_STUDENT,UserStatus.ACTIVE).then((res) => {
             setTotalStudents(res.data)
@@ -23,19 +21,14 @@ export const InstructorOverview = () => {
         }).catch((err) => {
             toast.error(err.message);
         })
-        new SemesterDao().getCurrentSemester().then((res) => {
-            setSemester(res.data)
-        }).catch((err) => {
-            toast.error(err.message);
-        })
     },[])
   return <>
   <div className="flex items-center justify-between border-t-4 border-t-blue-900 mt-5 p-2 border border-b-4 border-b-blue-200/50">
             <div className="border-r border-r-blue-200 pe-2">
                 <div className="text-slate-600 font-bold mb-2 ">
-                    Semester
+                    Semester  {prop.semester.semNumber==1?'I':prop.semester.semNumber==2?'II':prop.semester.semNumber=3?'III':prop.semester.semNumber==4?'IV':prop.semester.semNumber=5?'V':prop.semester.semNumber==6?'VI':prop.semester.semNumber=7?'VII':prop.semester.semNumber==8?'VIII':''}
                 </div>
-                <div className="text-sm font-bold">{semester.semesterName}</div>
+                <div className="text-sm font-bold">{prop.semester.semesterName}</div>
             </div>
             <DashboardCard icon={<People />} title="Students"
             path="/instructor/students"
