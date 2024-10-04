7d678fdb6464e0781a3f03e7a261f68bdd504556
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-10-04 06:32:15 +0200

    add semester information

--- src/page/instructor/index/index.tsx
+++ src/page/instructor/index/index.tsx
@@ -1,13 +1,28 @@
+import { useEffect, useState } from "react"
 import { Navigation } from "../../../component/navigation"
 import { InstructorMenu } from "../../../util/instructorMenu"
 import { IndexOverviewHeading } from "../../component"
 import { InstructorOverview } from "./instructoroverview"
 import { RecentCourse } from "./recentcourse"
+import { SemesterDao } from "../../../controller/semesterdao"
+import { toast } from "sonner"
 
 export const InstructorHome=()=>{
+    const [semester,setSemester]=useState({})
+    useEffect(
+        ()=>{
+            new SemesterDao().getCurrentSemester().then((res) => {
+                setSemester(res.data)
+            }).catch((err) => {
+                toast.error(err.message);
+            })
+        },[]
+    )
     return <Navigation items={InstructorMenu}>
+        <div className="overflow-x-hidden h-full">
         <IndexOverviewHeading/>
-        <InstructorOverview/>
-        <RecentCourse/>
+        <InstructorOverview semester={semester}/>
+        <RecentCourse semester={semester}/>
+        </div>
     </Navigation>
 }
