da9bae7609f994d5869225ad5a5bf3a6e749eb96
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-10-04 06:33:45 +0200

    remove semester route

--- src/util/hodMenu.tsx
+++ src/util/hodMenu.tsx
@@ -1,10 +1,8 @@
-import { Dashboard, Description, Folder, Grading, LockPerson, School } from "@mui/icons-material";
+import { Dashboard, Description,Grading, LockPerson, School } from "@mui/icons-material";
 import { INavigationItems } from "../interface/navigationitems";
 
 export const HodMenu:INavigationItems[]=[
     {icon:<Dashboard/>,link:'/hod',name:'Overview'},
-    {icon:<Folder/>,link:'/hod/semesters',name:'Semesters'},
-    {icon:<Folder/>,link:'/hod/department',name:'Department'},
     {icon:<LockPerson/>,link:'/hod/lectures',name:'lectures'},
     {icon:<School/>,link:'/hod/students',name:'Students'},
     {icon:<Description/>,link:'/hod/courses',name:'Courses'},
