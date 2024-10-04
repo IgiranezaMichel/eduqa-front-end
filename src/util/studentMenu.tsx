68355b93e43e68f5af95ecd315cbace7d830ab90
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-10-04 06:34:00 +0200

    add survey

--- src/util/studentMenu.tsx
+++ src/util/studentMenu.tsx
@@ -1,8 +1,9 @@
-import { BookSharp, Dashboard, Folder } from "@mui/icons-material";
+import { BookSharp, Dashboard, Edit, Folder } from "@mui/icons-material";
 import { INavigationItems } from "../interface/navigationitems";
 
 export const StudentMenu:INavigationItems[]=[
     {icon:<Dashboard/>,link:'/student',name:'Overview'},
     {icon:<Folder/>,link:'/student/semesters',name:'Semesters'},
-    {icon:<BookSharp/>,link:'/student/course',name:'Courses'}
+    {icon:<BookSharp/>,link:'/student/course',name:'Courses'},
+    {icon:<Edit/>,link:'/student/survey',name:'Survey'}
 ]
