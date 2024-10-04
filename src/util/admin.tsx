d0113b9e0a7b810e86645db16103ad4b9d811ad3
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-10-04 06:33:25 +0200

    add department route

--- src/util/admin.tsx
+++ src/util/admin.tsx
@@ -1,9 +1,11 @@
-import { BookSharp, Dashboard, Description, Folder, PeopleAlt } from "@mui/icons-material";
+import { BookSharp, Dashboard, Description, Folder, LocalPostOfficeRounded, PeopleAlt } from "@mui/icons-material";
 import { INavigationItems } from "../interface/navigationitems";
 
 export const AdminMenu:INavigationItems[]=[
     {icon:<Dashboard/>,link:'/admin',name:'Overview'},
     {icon:<PeopleAlt/>,link:'/admin/user',name:'Users'},
+    // {icon:<Folder/>,link:'/admin/semesters',name:'Semesters'},
+    {icon:<LocalPostOfficeRounded/>,link:'/admin/department',name:'Department'},
     {icon:<Folder/>,link:'/admin/semester',name:'Semesters'},
     {icon:<BookSharp/>,link:'/admin/course',name:'Courses'},
     {icon:<Description/>,link:'/admin/report',name:'Reports'},
