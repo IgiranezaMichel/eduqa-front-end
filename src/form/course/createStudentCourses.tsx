d41d46a4fd25509e22df118caf970a2afd676af4
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-10-14 21:39:32 +0200

    fill form

--- src/form/course/createStudentCourses.tsx
+++ src/form/course/createStudentCourses.tsx
@@ -1,11 +1,14 @@
-import { FC, ReactNode, useState } from "react";
-import { Button, TextField } from "@mui/material";
+import { FC, ReactNode, useEffect, useState } from "react";
+import { Button, FormControl, InputLabel, MenuItem, Select } from "@mui/material";
 import { toast, Toaster } from "sonner";
 import { IStudentRegisterCourse } from "../../interface/studentregistercourse";
 import { useStudentRegisterCourseWithInSemesterContext } from "../../context/studentregisteredcourseinsemester";
 import { StudentRegisterCourseDao } from "../../controller/studentregistercourse";
+import { LectureCourseDao } from "../../controller/lecturecourses";
+import { Person } from "@mui/icons-material";
 interface IStudentRegisterCourseItem {
     StudentRegistercourse: IStudentRegisterCourse,
+    semester:any
     children: ReactNode
 
 }
@@ -14,13 +17,26 @@ export const CreateStudentStudentRegisterCourse: FC<IStudentRegisterCourseItem> 
         id: prop.StudentRegistercourse.id,
         lectureCourseId: prop.StudentRegistercourse.lectureCourseId,
         registrationId: prop.StudentRegistercourse.registrationId,
-    })
+    });
+    const [lectureDetails,setLectureDetails]=useState<any>({});
+    const [lectureCourseList,setLectureCourseList]=useState<any>([]);
+    const [group,setGroup]=useState<any>([]);
     const {refresh}=useStudentRegisterCourseWithInSemesterContext();
     const saveStudentRegisterCourse = (e:any) => {
-        e.preventDefault();
+        e.preventDefault(); 
         new StudentRegisterCourseDao().addStudentCourse(StudentRegistercourse).then(
             data=>{toast.success(data.data);refresh()}
         ).catch(err=>toast.error(err.response.data))
+    }
+    useEffect(
+    ()=>{
+   new LectureCourseDao().getAllLectureCourseAvailableWitnInASemester(prop.semester)
+   .then(data=>setLectureCourseList(data.data));
+    },[]
+    )
+    const findGroup=(courseId:string)=>{
+        new LectureCourseDao().getAllActiveCourseGroups(courseId,prop.semester)
+        .then(data=>setGroup(data.data)).catch(err=>console.log(err));
     }
     return <>
         <div>
@@ -28,25 +44,46 @@ export const CreateStudentStudentRegisterCourse: FC<IStudentRegisterCourseItem> 
                 {prop.children}
             </div>
             <form className="p-2" onSubmit={saveStudentRegisterCourse}>
-                <TextField required label='Module Code' value={StudentRegistercourse.code}
-                    onChange={(e) => setStudentRegisterCourse({ ...StudentRegistercourse, code: e.target.value })} className="mb-5" fullWidth />
-                <TextField required label='Module name' value={StudentRegistercourse.name}
-                    onChange={(e) => setStudentRegisterCourse({ ...StudentRegistercourse, name: e.target.value })} className="mb-5" fullWidth />
-                <TextField required label='Module Credit' value={StudentRegistercourse.credit}
-                    onChange={(e) => setStudentRegisterCourse({ ...StudentRegistercourse, credit: Number(e.target.value) })} className="mb-5" fullWidth />
-{/*                 
+                {Object.keys(lectureDetails).length!=0&&<div className="flex gap-3 mb-5">
+                    <img src={lectureDetails.picture} alt="" />
+                    <section>
+                    <div className="flex gap-2 items-center mb-2"><Person/>{lectureDetails.code}</div>
+                    <div className="flex gap-2 items-center mb-2"><Person/>{lectureDetails.name}</div>
+                    <div className="flex gap-2 items-center mb-2"><Person/>{lectureDetails.gender}</div>
+                    <div className="flex gap-2 items-center mb-2"><Person/>{lectureDetails.phoneNumber}</div>
+                    <div className="flex gap-2 items-center mb-2"><Person/>{lectureDetails.email}</div>
+                    <div className="flex gap-2 items-center mb-2"><Person/>{lectureDetails.departmentName}</div>
+                    <div className="flex gap-2 items-center mb-2"><Person/>{lectureDetails.status}</div>
+                    </section>
+                    </div>}
                 <FormControl fullWidth>
-                    <InputLabel id="demo-simple-select-label">Select StudentRegistercourse StudentRegisterCourse Category</InputLabel>
+                    <InputLabel id="demo-simple-select-label">Select Course</InputLabel>
                     <Select
-                        labelId="demo-simple-select-label"
+                        labelId="Select Course"
                         id="demo-simple-select"
-                        value={StudentRegistercourse.type}
-                        label="Select StudentRegistercourse StudentRegisterCourse Category"
+                        onChange={(e)=>findGroup(e.target.value as string)}
+                        label="Select Course"
                     >
-                        <MenuItem value={StudentRegisterCourseCategory.GENERAL}>General StudentRegisterCourse</MenuItem>
-                        <MenuItem value={StudentRegisterCourseCategory.CORE}>Core StudentRegisterCourse</MenuItem>
+                       {lectureCourseList!=undefined&&lectureCourseList.length!=0&&
+                       lectureCourseList.map ((data:any)=><MenuItem key={data.id} value={data.course.id}>
+                       {data.course.name}
+                       </MenuItem>)}
                      </Select>
-                </FormControl> */}
+                </FormControl>
+                {group!=undefined&&group.length!=0&&<FormControl fullWidth className="mt-4">
+                    <InputLabel id="demo-simple-select-label">Select Group</InputLabel>
+                    <Select
+                        labelId="Select Course"
+                        id="demo-simple-select"
+                        onChange={(e)=>{setLectureDetails(e.target.value);setStudentRegisterCourse({...StudentRegistercourse,lectureCourseId:lectureDetails.id})}}
+                        label="Select Course"
+                    >
+                       {group!=undefined&&group.length!=0&&
+                       group.map ((data:any)=><MenuItem key={data.id} value={data}>
+                       {data.group}
+                       </MenuItem>)}
+                     </Select>
+                </FormControl>}
                 <div className="py-2 ">
                     <Toaster/>
                 <Button type="submit" className="bg-blue-950/90 w-full normal-case text-white p-3 font-bold text-xl">Add new StudentRegistercourse</Button>
