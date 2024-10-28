8257745a55599e1e6fdccfdead7e52fa4fe7f770
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-10-28 21:53:05 +0200

    add questions

--- src/form/review/create.tsx
+++ src/form/review/create.tsx
@@ -1,8 +1,28 @@
-export const ReviewForm=()=>{
+import { ReactNode } from "react"
+
+export const ReviewForm=(prop:{lectureCourse:any,children:ReactNode})=>{
     return <>
     <form action="">
-        <div className="grid grid-cols-2">
-            <div>What i</div>
+        {prop.children}
+        <div className="grid grid-cols-2 gap-5 p-1 bg-blue-600 text-white">
+            <div>Question</div>
+            <section className="flex justify-between">
+                <div>Bad</div>
+                <div>Neutral </div>
+                <div>Good</div>
+                <div>Better</div>
+                <div>Best</div>
+            </section>
+        </div>
+        <div className="grid grid-cols-2 gap-5 p-1">
+            <div>What is abc ewgwe gregher htretyhrt eyeyer</div>
+            <section className="flex justify-between text-center">
+                <input type="checkbox" name="" id="" />
+                <input type="checkbox" name="" id="" />
+                <input type="checkbox" name="" id="" />
+                <input type="checkbox" name="" id="" />
+                <input type="checkbox" name="" id="" />
+            </section>
         </div>
     </form>
     </>
