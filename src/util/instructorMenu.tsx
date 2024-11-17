f0ae46c2bfab8024fa783ba434b2b74728a44bf3
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-11-17 16:29:42 +0200

    remove semester

--- src/util/instructorMenu.tsx
+++ src/util/instructorMenu.tsx
@@ -1,9 +1,9 @@
-import { BookSharp, Dashboard, Folder,People } from "@mui/icons-material";
+import { BookSharp, Dashboard,People } from "@mui/icons-material";
 import { INavigationItems } from "../interface/navigationitems";
 
 export const InstructorMenu:INavigationItems[]=[
     {icon:<Dashboard/>,link:'/instructor',name:'Overview'},
-    {icon:<Folder/>,link:'/instructor/semesters',name:'Semesters'},
+    // {icon:<Folder/>,link:'/instructor/semesters',name:'Semesters'},
     {icon:<People/>,link:'/instructor/student',name:'Student'},
     {icon:<BookSharp/>,link:'/instructor/course',name:'Courses'}
 ]
